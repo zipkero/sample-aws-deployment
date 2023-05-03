0be81e7fee080e3377974d50339fd789fa61c427
zipkero <zipkero@gmail.com>
2023-05-03 23:58:06 +0900

    added docker-compose.yaml

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
      ```
 2. WAS Docker Image 생성
    - Dockerfile 작성
-     ```yaml
+     ```text
      FROM node:6.0
      COPY . .
      RUN npm i
@@ -20,13 +20,26 @@
      ```
    - .dockerignore 작성
 3. WAS Docker Image 빌드 및 실행
-   ```
+   ```bash
    docker build -t sample-aws-was:1.0 ./sample-aws-was
    docker run -itd --name sample-aws-was -p 3000:3000 sample-aws-was:1.0
    ```
 4. DB Docker Image 생성
    - Dockerfile 작성
+     ```text
+     FROM mongo:6.0
+     COPY ./init-mongo.js ./docker-entrypoint-initdb.d/
+     ENV MONGO_INITDB_ROOT_USERNAME=root \
+         MONGO_INITDB_ROOT_PASSWORD=1q2w3e
+     ```
 5. WAS, DB Container 실행 후 연결
+
+   - docker-compose.yaml 작성
+
+     ```yaml
+
+     ```
+
 6. AWS ECR 레파지토리 생성 후 이미지 푸시
 7. Github Actions를 이용한 이미지 푸시 자동화
 8. ECR 이미지를 이용한 ECS 컨테이너 세팅
--- sample-aws-was/src/app.ts
+++ sample-aws-was/src/app.ts
@@ -1,21 +1,31 @@
-import { join } from 'path';
-import AutoLoad, {AutoloadPluginOptions} from '@fastify/autoload';
-import { FastifyPluginAsync } from 'fastify';
+import { join } from "path";
+import AutoLoad, { AutoloadPluginOptions } from "@fastify/autoload";
+import { FastifyPluginAsync } from "fastify";
+import { fastifyMongodb } from "@fastify/mongodb";
+
+const { MONGO_USER, MONGO_PASSWORD, MONGO_DATABASE, MONGO_HOST, MONGO_PORT } =
+  process.env;
 
 export type AppOptions = {
   // Place your custom options for app below here.
+  mongoUrl: string;
 } & Partial<AutoloadPluginOptions>;
-
 
 // Pass --options via CLI arguments in command to enable these options.
 const options: AppOptions = {
-}
+  mongoUrl: `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`,
+};
 
 const app: FastifyPluginAsync<AppOptions> = async (
-    fastify,
-    opts
+  fastify,
+  opts
 ): Promise<void> => {
   // Place here your custom code!
+  if (opts.mongoUrl) {
+    void fastify.register(fastifyMongodb, {
+      url: opts.mongoUrl,
+    });
+  }
 
   // Do not touch the following lines
 
@@ -23,18 +33,17 @@ const app: FastifyPluginAsync<AppOptions> = async (
   // those should be support plugins that are reused
   // through your application
   void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'plugins'),
-    options: opts
-  })
+    dir: join(__dirname, "plugins"),
+    options: opts,
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'routes'),
-    options: opts
-  })
-
+    dir: join(__dirname, "routes"),
+    options: opts,
+  });
 };
 
 export default app;
-export { app, options }
+export { app, options };
